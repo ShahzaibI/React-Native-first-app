fd66c635c4f728bcb4e80d1e0ba01061ef5fbd6a
ShahzaibI <sg03056924122@gmail.com>
2026-02-17 12:34:47 +0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import { View, Text, Button, StyleSheet } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>App</Text>
+      <Button title='Press Here' color='#007AFF'/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    padding: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+})
+
+export default App
